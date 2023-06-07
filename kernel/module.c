6f8664fee734031231c263d3e4eee81ede81c9da
Nobody6825 <117302867+Nobooooody@users.noreply.github.com>
2023-06-07 13:06:28 +0000

    Modify defconfig for docker and lxc

--- kernel/module.c
+++ kernel/module.c
@@ -3703,7 +3703,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err = 0;
 	char *after_dashes;
-	
+
 	//FIXME
 	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
 	flags |= MODULE_INIT_IGNORE_VERMAGIC;
